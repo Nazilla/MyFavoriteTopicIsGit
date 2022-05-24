678e456c6cbb2455de245ce8f204cb04279d1236
Nazilla <nazilla.sultani@gmail.com>
2022-05-24 02:17:24 -0400

    this is LabPractice

---
+++ src/test/java/us/tekschool/git/practice/us/tekschool/git/practice/GitPom.java
@@ -0,0 +1,10 @@
+package us.tekschool.git.practice.us.tekschool.git.practice;
+
+public class GitPom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/us/tekschool/git/practice/us/tekschool/git/practice/GitStepDef.java
@@ -0,0 +1,10 @@
+package us.tekschool.git.practice.us.tekschool.git.practice;
+
+public class GitStepDef {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
